a3498a4e3b489468dc5a8201df2b1040c3049097
unknown <mi-kim@LT-E5570-1460.ORANGE>
2020-02-27 11:45:12 +0900

    start

---
+++ StampWF/CreateStamp.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace StampWF
+{
+    partial class CreateStamp
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.chkDate = new System.Windows.Forms.CheckBox();
+            this.txtFamilyName = new System.Windows.Forms.TextBox();
+            this.txtGivenName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(182, 96);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(90, 71);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "作成";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // chkDate
+            // 
+            this.chkDate.AutoSize = true;
+            this.chkDate.Checked = true;
+            this.chkDate.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDate.Location = new System.Drawing.Point(12, 38);
+            this.chkDate.Name = "chkDate";
+            this.chkDate.Size = new System.Drawing.Size(15, 14);
+            this.chkDate.TabIndex = 1;
+            this.chkDate.UseVisualStyleBackColor = true;
+            // 
+            // txtFamilyName
+            // 
+            this.txtFamilyName.Location = new System.Drawing.Point(35, 11);
+            this.txtFamilyName.Name = "txtFamilyName";
+            this.txtFamilyName.Size = new System.Drawing.Size(100, 19);
+            this.txtFamilyName.TabIndex = 2;
+            // 
+            // txtGivenName
+            // 
+            this.txtGivenName.Location = new System.Drawing.Point(170, 11);
+            this.txtGivenName.Name = "txtGivenName";
+            this.txtGivenName.Size = new System.Drawing.Size(100, 19);
+            this.txtGivenName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "姓：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "名：";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(143, 37);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "透明背景";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "＊ファイルはマイピクチャーフォルダに入ります。";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(60, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(210, 20);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "フォント：";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(33, 36);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(102, 19);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(182, 173);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 71);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // CreateStamp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 274);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGivenName);
+            this.Controls.Add(this.txtFamilyName);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.chkDate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "CreateStamp";
+            this.Text = "StampWF";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.CheckBox chkDate;
+        private System.Windows.Forms.TextBox txtFamilyName;
+        private System.Windows.Forms.TextBox txtGivenName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ StampWF/CreateStamp.cs
@@ -0,0 +1,163 @@
+﻿//------------------------------------------------------------------------------
+//     StampWF
+//     Created 2016/4/25 by mi-kim@teratech.co.jp
+//
+//     機能①：日付表示選択(追加orなし)
+//     機能②：背景選択(白or透明)
+//     機能③：フォント選択
+//     機能④：プレビュー
+//     
+//     名字　１桁：上、２桁：上１下１、３桁：上２下１、４桁：上２下２
+//
+//     名前の入力可能
+//     名字が３桁以上の場合、名前は入力不可
+//     名前が３桁以上の場合、頭２桁のみ出力
+//
+//     空白入力不可
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StampWF
+{
+    public partial class CreateStamp : Form
+    {
+        public CreateStamp()
+        {
+            InitializeComponent();
+
+            Bitmap stampImg = new Bitmap(iImageSize, iImageSize);
+            pictureBox1.Image = stampImg;
+        }
+
+        const int iImageSize = 150;
+        const int iPadding = 3;
+        const int iCircleSize = iImageSize - iPadding*2;
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string strFamilyName = "";
+            string strGivenName = "";
+
+            strFamilyName = txtFamilyName.Text.ToString();
+            strGivenName = txtGivenName.Text.ToString();
+
+            Graphics textGraphics = Graphics.FromHwnd(txtFamilyName.Handle);
+            SizeF stringSize = textGraphics.MeasureString(txtFamilyName.Text, txtFamilyName.Font);
+
+            MessageBox.Show(System.Environment.SystemDirectory);
+
+            //MessageBox.Show(stringSize.Width.ToString());
+
+            //int iPosition = (iCircleSize - (int)stringSize.Width) / 2;
+            //int iLength = strFamilyName.Length;
+
+            //int iPosition = (iCircleSize - 40) / 2 - 20;
+
+            //MessageBox.Show(iPosition.ToString());
+            //if (stringSize.Width
+
+            //if (strFamilyName.Equals("")&& strFamilyName.Equals(""))
+            //    return;
+
+            string strStampDate = dateTimePicker1.Value.ToString("yyyy.MM.dd");
+
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            if (strGivenName.Equals(""))
+            {
+                try
+                {
+                    g.DrawImage(pictureBox1.Image, 0, 0, iImageSize, iImageSize);
+                    g.FillRectangle(Brushes.White, new Rectangle(0, 0, iImageSize, iImageSize));
+
+                    Pen pen = new Pen(Color.Red, 4);
+                    g.DrawEllipse(pen, iPadding, iPadding, iCircleSize, iCircleSize);
+
+                    if (chkDate.Checked)
+                    {
+                        pen = new Pen(Color.Red, 3);
+                        g.DrawLine(pen, new Point(5, 80), new Point(145, 80));
+                        g.DrawLine(pen, new Point(12, 110), new Point(138, 110));
+                        g.DrawString(strStampDate, new Font("MS UI Gothic", 20), Brushes.Red, 9, 81);
+                    }
+
+                    Font fontChar = new Font("ＭＳ Ｐ明朝", 40, FontStyle.Bold);
+
+                    g.DrawString(strFamilyName, fontChar, Brushes.Red, 40, 19);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    if (g != null)
+                        g.Dispose();
+
+                    //if (stampImg != null)
+                    //    stampImg.Dispose();
+
+                    pictureBox1.Refresh();
+                }
+            }
+            else
+            {
+                try
+                {
+                    g.DrawImage(pictureBox1.Image, 0, 0, iImageSize, iImageSize);
+                    g.FillRectangle(Brushes.White, new Rectangle(0, 0, iImageSize, iImageSize));
+
+                    Pen pen = new Pen(Color.Red, 4);
+                    g.DrawEllipse(pen, iPadding, iPadding, iCircleSize, iCircleSize);
+
+                    if (chkDate.Checked)
+                    {
+                        pen = new Pen(Color.Red, 3);
+                        g.DrawLine(pen, new Point(5, 60), new Point(145, 60));
+                        g.DrawLine(pen, new Point(5, 90), new Point(145, 90));
+                        g.DrawString(strStampDate, new Font("MS UI Gothic", 20), Brushes.Red, 9, 61);
+                    }
+
+                    Font fontChar = new Font("ＭＳ Ｐ明朝", 32, FontStyle.Bold);
+                    g.DrawString(strFamilyName, fontChar, Brushes.Red, 46, 11);
+                    //Font fontCharGiven = new Font("ＭＳ Ｐ明朝", 30, FontStyle.Bold);
+                    //g.DrawString(strGivenName, fontCharGiven, Brushes.Red, 26, 96);
+
+                    Font fontCharGiven = new Font("ＭＳ Ｐ明朝", 32, FontStyle.Bold);
+                    g.DrawString(strGivenName, fontCharGiven, Brushes.Red, 46, 96);
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    if (g != null)
+                        g.Dispose();
+
+                    //if (stampImg != null)
+                    //    stampImg.Dispose();
+
+                    pictureBox1.Refresh();
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string strStampFileName = "StampWF" + System.DateTime.Now.ToString("yyyyMMddhhmmss") + ".png";
+
+            pictureBox1.Image.Save(strStampFileName, System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
